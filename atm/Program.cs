b74171823f18544f426573601c81f8a54f847728
KamranG12 <kamran.Jabiyev@aplusa-security.com>
2017-05-05 00:37:26 +0400

    atm

---
+++ atm/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   int mebleg=466;
+            int[] a = new int[5] { 100, 50, 20, 10, 5 };
+            if(mebleg%5!=0){
+                    Console.WriteLine("atm.de"+" "+"yalniz"+" "+"5,10,20,50,100"+" "+"Azn"+" "+"eskinaslar"+" "+"movcuddur");
+                }else{
+            foreach (int i in a)
+            { 
+                if(mebleg>=i){
+                    Console.WriteLine(i+" "+"eskinasindan"+"-"+mebleg/i+" "+"dene");
+                }
+                mebleg=mebleg%i;
+            }
+            Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+        
+            
+        
+    }
+}
